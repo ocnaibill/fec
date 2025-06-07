95a9f91505a068e03de2a8e16563a2a3a1c83b8d
Jhon Wilker <jhonwilker0112@gmail.com>
2025-06-07 13:23:47 -0300

    implementado rota para registro de usuarios

--- users/serializers.py
+++ users/serializers.py
@@ -2,11 +2,19 @@ from rest_framework import serializers
 from users.models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta(object):
         model = CustomUser
         fields = [ 
             'name', 
             'email', 
             'cpf', 
+            'birthdate',
+            'institution',
+            'registration_number',
             'password'
         ]
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
--- users/views.py
+++ users/views.py
@@ -14,7 +14,15 @@ def login(request):
 
 @api_view(['POST'])
 def signup(request):
-    return Response({})
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+        user = CustomUser.objects.get(email=request.data['email'])
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def logout(request):
