475497162ab3ae05b89cd13197dcc9f71d422add
Jhon Wilker <jhonwilker0112@gmail.com>
2025-06-07 22:19:20 -0300

    feature: implementação de rotas para visualização separada de palestras e oficinas

--- events/urls.py
+++ events/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.create_event, name='create_event'),
     path('<int:event_pk>/', views.list_event_details, name='event_activities'),
     path('lecture/', views.add_lecture_to_event, name='create_lecture'),
+    path('lecture/<int:event_pk>/', views.list_event_lectures, name='event_lectures'),
     path('workshop/', views.add_workshop_to_event, name='create_workshop'),
+    path('workshop/<int:event_pk>/', views.list_event_workshops, name='event_workshops'),
     path('subscribe/', views.subscribe_to_event, name='subscribe'),
 ]
--- events/views.py
+++ events/views.py
@@ -38,6 +38,16 @@ def add_lecture_to_event(requests):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+def list_event_lectures(requests, event_pk):
+    try:
+        lectures = Lecture.objects.filter(event_id=event_pk)
+    except Lecture.DoesNotExist:
+        return Response({'erro': 'Nenhuma palestra associada ao evento.'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = LectureSerializer(lectures, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 # OFICINAS
 @api_view(['POST'])
 def add_workshop_to_event(requests):
@@ -47,6 +57,16 @@ def add_workshop_to_event(requests):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def list_event_workshops(requests, event_pk):
+    try:
+        workshop = Workshop.objects.filter(event_id=event_pk)
+    except Workshop.DoesNotExist:
+        return Response({'erro': 'Nenhuma oficina associada ao evento.'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = WorkshopSerializer(workshop, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 # INSCRIÇÕES
 @api_view(['POST'])
