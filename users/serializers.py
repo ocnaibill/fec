61de254cbf03537ca5ab73c5a1dae473a7a7a3f3
Jhon Wilker <jhonwilker0112@gmail.com>
2025-06-11 09:47:08 -0300

    add: validação do campo matricula caso instuição seja UCB

--- users/serializers.py
+++ users/serializers.py
@@ -18,3 +18,10 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
+    
+    def validate(self, data):
+        if data.get('institution') == 'UCB' and not data.get('registration_number'):
+            raise serializers.ValidationError({
+                'registration_number': 'Número de matrícula é obrigatório para UCB.'
+            })
+        return data
