692c403feea5044a1d4b6149ded9574d17bdc73b
ocanibill <biancocosta01@gmail.com>
2025-06-11 11:46:30 -0300

    update: adding /media route for static resources

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from core.views import hello
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('api/hello/', hello),
     path('api/auth/', include('users.urls')),
     path('api/event/', include('events.urls')),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
