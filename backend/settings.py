42d3baa583e95f384434012ca4d47c74309bbd00
Jhon Wilker <jhonwilker0112@gmail.com>
2025-06-19 17:07:56 -0300

    add: configurações do dominio SMTP

--- backend/settings.py
+++ backend/settings.py
@@ -132,6 +132,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
