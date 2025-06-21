8d8968b1905168830d86667d7d48e6ad07be153d
Jhon Wilker <jhonwilker0112@gmail.com>
2025-06-21 14:56:39 -0300

    feature: criação de qrcodes para inscrições

--- events/models.py
+++ events/models.py
@@ -3,6 +3,11 @@ from users.models import CustomUser
 from django.utils import timezone
 from django.core.exceptions import ValidationError
 
+from django.core.files.base import ContentFile
+from io import BytesIO
+import uuid
+import qrcode
+
 # Create your models here.
 
 def validate_svg_or_image(file):
@@ -60,10 +65,24 @@ class StatusSubscription(models.TextChoices):
     CONFLITANT = 'conflitante', 'Conflitante'
 
 class Subscription(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
     status = models.CharField(max_length=12, choices=StatusSubscription.choices, default=StatusSubscription.EMITED)
+    qrcode = models.ImageField(upload_to='subscriptions-qrcodes/', blank=True, null=True)
     created_at = models.DateTimeField(default=timezone.now)
+
+    def generate_qrcode(self):
+        qr = qrcode.make(self.uuid)
+        buffer = BytesIO()
+
+        qr.save(buffer, format='PNG')
+        self.qrcode.save(f'{self.uuid}.png', ContentFile(buffer.getvalue()), save=False)
+
+    def save(self, *args, **kwargs):
+        if not self.qrcode:
+            self.generate_qrcode()
+        super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'subscriptions'
